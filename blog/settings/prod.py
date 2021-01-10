c762eb64a839e89dd8924df5b8c40d326ceea82d
Andre Zair Aspajo Escobar <Azae@MacBook-Air-de-Andre.local>
2021-01-10 11:57:57 -0500

    Update prod con ip server

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','http://68.183.153.36/','68.183.153.36']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
